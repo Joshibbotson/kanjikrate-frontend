556515a1c6eacaf665a4c195f0ab92067251048d
Josh Ibbotson <joshua_ibbotson@hotmail.com>
2024-07-04 07:31:13 +0100

    basic outline of deck cover

--- src/app/deck/deck.component.ts
+++ src/app/deck/deck.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, InputSignal, Signal, input } from '@angular/core';
 import { DeckCardComponent } from '../ui/deck-card/deckCard.component';
+import { Deck, DeckService } from '../api';
+import { ActivatedRoute } from '@angular/router';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-deck',
@@ -8,4 +11,30 @@ import { DeckCardComponent } from '../ui/deck-card/deckCard.component';
   templateUrl: './deck.component.html',
   styleUrl: './deck.component.scss',
 })
-export class DeckComponent {}
+export class DeckComponent {
+  public deckData: Deck | undefined;
+  constructor(
+    private readonly deckService: DeckService,
+    private readonly activatedRoute: ActivatedRoute
+  ) {
+    this.fetchDeckById();
+  }
+
+  // in general we need some kind of loading mechanism
+  private fetchDeckById() {
+    const id = this.activatedRoute.snapshot.params['id'];
+    console.log(id);
+    this.deckService
+      .readDeckById(id)
+      .pipe(
+        catchError((err) => {
+          throw new Error(err);
+        })
+      )
+      .subscribe((res) => {
+        if (res.success) {
+          this.deckData = res.data;
+        }
+      });
+  }
+}
